44f717a5901aeb8179f0bcd91553fd1d4aa88392
jwilliampope <jwilliampope@hotmail.com>
2021-03-18 16:28:01 -0400

    project complete

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
 const entryForm = document.getElementById("entryForm")
-const entriesSection = document.getElementById("entries")
-const entryTextbox = document.getElementsByClassName("entry-textbox")
+const entriesSection = document.querySelector("#entries")
+const entryTextbox = document.querySelector(".entry-textbox")
+const entriesNav = document.querySelector(".entries-nav")
+
+let count = 1
 
 function addEntryToDom(e) {
   e.preventDefault()
+  const entryDiv = document.createElement("div")
+  entryDiv.className = "single-entry"
+  entryDiv.innerText = entryTextbox.value
+  entryDiv.style.display = "none"
+  entriesSection.appendChild(entryDiv)
+  entryTextbox.value = ""
+
+  const displayEntryButton = document.createElement("button")
+  displayEntryButton.className = "display-entry-button"
+  displayEntryButton.innerText = count
+  entriesNav.appendChild(displayEntryButton)
+
+  displayEntryButton.addEventListener("click", () => {
+    const allEntries = document.querySelectorAll(".single-entry")
+
+    for (let i = 0; i < allEntries.length; i++) {
+      allEntries[i].style.display = "none"
+    }
+    entryDiv.style.display = "block"
+  })
+  count++
 }
 
 entryForm.addEventListener("submit", addEntryToDom)
